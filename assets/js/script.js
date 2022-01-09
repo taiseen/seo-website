c08c60f7d409273814c9de4aa971c5cde7220227
taiseen <taiseen.cse@gmail.com>
2022-01-09 06:07:32 +0600

    menubar auto close after click | js ==> OK

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,20 @@ const HtmlBody = document.body;
 const themeBtn = document.getElementById('theme-btn');
 const menuBtn = document.getElementById('menu-btn');
 const navbar = document.querySelector('.navbar');
+const navbarLink = document.querySelectorAll('.navbar a');
+
 
 menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('fa-times');
     navbar.classList.toggle('userClick');
 });
 
+navbarLink.forEach(link => {
+    link.addEventListener('click', () => {
+        // you are awesome... <3
+        navbar.classList.remove('userClick');
+    });
+});
 
 themeBtn.addEventListener('click', () => {
 
